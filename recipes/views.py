9eb3b3e944e032911dcbc6fa22c6809f9fe352ac
Erickson Dutra <ericksond10@gmail.com>
2022-01-25 23:25:22 -0400

    Herança, blocos, if

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio',
+        'name': 'Erickson Dutra',
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Erickson',
     })
