400fe9d3117c40b7eba310d8ef90e9f6493b90c0
Erickson Dutra <ericksond10@gmail.com>
2022-02-12 11:03:46 -0400

    test search_term

--- recipes/tests/test_recipe_view.py
+++ recipes/tests/test_recipe_view.py
@@ -135,10 +135,18 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIs(resolved.func, views.search)
 
     def test_recipe_search_loads_correct_template(self):
-        response = self.client.get(reverse('recipes:search'))
+        response = self.client.get(reverse('recipes:search') + '?q=test')
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
 
     def test_recipe_search_raises_404_if_no_search_term(self):
         url = reverse('recipes:search')  # + '?q=test'
         response = self.client.get(url)
         self.assertEquals(response.status_code, 404)
+
+    def test_recipe_search_term_is_on_page_title_and_escaped(self):
+        url = reverse('recipes:search') + '?q=<Test>'
+        response = self.client.get(url)
+        self.assertIn(
+            ' Search for &quot;&lt;Test&gt; | &quot',
+            response.content.decode('utf-8')
+        )
