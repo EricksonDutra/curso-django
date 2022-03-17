64b0f375af09ef7f6943333761e67d9c671c133d
Erickson Dutra <ericksond10@gmail.com>
2022-03-17 19:30:07 -0400

    login_screen

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -16,7 +16,7 @@ class RecipeSearchViewTest(RecipeTestBase):
     def test_recipe_search_raises_404_if_no_search_term(self):
         url = reverse('recipes:search')  # + '?q=test'
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 404)
+        self.assertEqual(response.status_code, 404)
 
     def test_recipe_search_term_is_on_page_title_and_escaped(self):
         url = reverse('recipes:search') + '?q=<Teste>'
