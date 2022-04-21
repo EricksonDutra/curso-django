6eea2d5e39ecc9277d4cba498b522fe6bac842ec
EricksonDutra <ericksond10@gmail.com>
2022-04-21 15:00:11 -0400

    return

--- utils/browser copy.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-from selenium import webdriver
-
-ROOT_PATH = Path(__file__).parent.parent
-CHROMEDRIVER_NAME = 'chromedriver'
-CHROMEDRIVER_PATH = ROOT_PATH / 'bin' / CHROMEDRIVER_NAME
