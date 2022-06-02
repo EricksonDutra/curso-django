6300170eb5b62d9b9f2a6ebd78776e4df6019831
EricksonDutra <ericksond10@gmail.com>
2022-06-01 20:09:48 -0400

    Fix ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- project/settings.py
+++ project/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = parse_comma_sep_str_to_list(
-    get_env_variable('ALLOWED_HOST'))  # type:  ignore
+    get_env_variable('ALLOWED_HOSTS'))  # type:  ignore
 CSRF_TRUSTED_ORIGINS = parse_comma_sep_str_to_list(
     get_env_variable('CSRF_TRUSTED_ORIGINS')
 )
