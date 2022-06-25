d1173fb1ee71760158356e175fdea5cbc6af7119
EricksonDutra <ericksond10@gmail.com>
2022-06-25 10:14:14 -0400

    final course

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,107 +1,196 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
-from tag.models import Tag
+from unittest.mock import patch
 
-from ..models import Recipe
-from ..permissions import IsOwner
-from ..serializers import RecipeSerializer, TagSerializer
+from django.urls import reverse
+from recipes.tests.test_recipe_base import RecipeMixin
+from rest_framework import test
 
 
-class RecipeAPIv2Pagination(PageNumberPagination):
-    page_size = 2
+class RecipeAPIv2TestMixin(RecipeMixin):
+    def get_recipe_list_reverse_url(self, reverse_result=None):
+        api_url = reverse_result or reverse('recipes:recipes-api-list')
+        return api_url
 
+    def get_recipe_api_list(self, reverse_result=None):
+        api_url = self.get_recipe_list_reverse_url(reverse_result)
+        response = self.client.get(api_url)
+        return response
 
-class RecipeAPIv2ViewSet(ModelViewSet):
-    queryset = Recipe.objects.get_published()
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeAPIv2Pagination
-    permission_classes = [IsAuthenticatedOrReadOnly, ]
-    http_method_names = ['get', 'options', 'head', 'patch', 'post', 'delete']
-
-    def get_serializer_class(self):
-        return super().get_serializer_class()
-
-    def get_serializer(self, *args, **kwargs):
-        return super().get_serializer(*args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["example"] = 'this is in context now'
-        return context
-
-    def get_queryset(self):
-        qs = super().get_queryset()
-
-        category_id = self.request.query_params.get('category_id', '')
-
-        if category_id != '' and category_id.isnumeric():
-            qs = qs.filter(category_id=category_id)
-
-        return qs
-
-    def get_object(self):
-        pk = self.kwargs.get('pk', '')
-
-        obj = get_object_or_404(
-            self.get_queryset(),
-            pk=pk,
+    def get_auth_data(self, username='user', password='pass'):
+        userdata = {
+            'username': username,
+            'password': password,
+        }
+        user = self.make_author(
+            username=userdata.get('username'),
+            password=userdata.get('password')
         )
-
-        self.check_object_permissions(self.request, obj)
-
-        return obj
-
-    def get_permissions(self):
-        if self.request.method in ['PATCH', 'DELETE']:
-            return [IsOwner(), ]
-        return super().get_permissions()
-
-    def list(self, request, *args, **kwargs):
-        print('REQUEST', request.user)
-        print(request.user.is_authenticated)
-        return super().list(request, *args, **kwargs)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(author=request.user)
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED,
-            headers=headers
+        response = self.client.post(
+            reverse('recipes:token_obtain_pair'), data={**userdata}
         )
+        return {
+            'jwt_access_token': response.data.get('access'),
+            'jwt_refresh_token': response.data.get('refresh'),
+            'user': user,
+        }
 
-    def partial_update(self, request, *args, **kwargs):
-        recipe = self.get_object()
-        serializer = RecipeSerializer(
-            instance=recipe,
-            data=request.data,
-            many=False,
-            context={'request': request},
-            partial=True,
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(
-            serializer.data,
+    def get_recipe_raw_data(self):
+        return {
+            'title': 'This is the title',
+            'description': 'This is the description',
+            'preparation_time': 1,
+            'preparation_time_unit': 'Minutes',
+            'servings': '1',
+            'servings_unit': 'Person',
+            'preparation_steps': 'This is the preparation steps.'
+        }
+
+    def test_recipe_api_list_returns_status_code_200(self):
+        response = self.get_recipe_api_list()
+        self.assertEqual(
+            response.status_code,
+            200
         )
 
 
-@api_view()
-def tag_api_detail(request, pk):
-    tag = get_object_or_404(
-        Tag.objects.all(),
-        pk=pk
-    )
-    serializer = TagSerializer(
-        instance=tag,
-        many=False,
-        context={'request': request},
-    )
-    return Response(serializer.data)
+class RecipeAPIv2Test(test.APITestCase, RecipeAPIv2TestMixin):
+    @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=7)
+    def test_recipe_api_list_loads_correct_number_of_recipes(self):
+        wanted_number_of_recipes = 7
+        self.make_recipe_in_batch(qtd=wanted_number_of_recipes)
+
+        response = self.client.get(
+            reverse('recipes:recipes-api-list') + '?page=1'
+        )
+        qtd_of_loaded_recipes = len(response.data.get('results'))
+
+        self.assertEqual(
+            wanted_number_of_recipes,
+            qtd_of_loaded_recipes
+        )
+
+    def test_recipe_api_list_do_not_show_not_published_recipes(self):
+        recipes = self.make_recipe_in_batch(qtd=2)
+        recipe_not_published = recipes[0]
+        recipe_not_published.is_published = False
+        recipe_not_published.save()
+        response = self.get_recipe_api_list()
+        self.assertEqual(
+            len(response.data.get('results')),
+            1
+        )
+
+    @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=10)
+    def test_recipe_api_list_loads_recipes_by_category_id(self):
+        # Creates categories
+        category_wanted = self.make_category(name='WANTED_CATEGORY')
+        category_not_wanted = self.make_category(name='NOT_WANTED_CATEGORY')
+
+        # Creates 10 recipes
+        recipes = self.make_recipe_in_batch(qtd=10)
+
+        # Change all recipes to the wanted category
+        for recipe in recipes:
+            recipe.category = category_wanted
+            recipe.save()
+
+        # Change one recipe to the NOT wanted category
+        # As a result, this recipe should NOT show in the page
+        recipes[0].category = category_not_wanted
+        recipes[0].save()
+
+        # Action: get recipes by wanted category_id
+        api_url = reverse('recipes:recipes-api-list') + \
+            f'?category_id={category_wanted.id}'
+        response = self.get_recipe_api_list(reverse_result=api_url)
+
+        # We should only see recipes from the wanted category
+        self.assertEqual(
+            len(response.data.get('results')),
+            9
+        )
+
+    def test_recipe_api_list_user_must_send_jwt_token_to_create_recipe(self):
+        api_url = self.get_recipe_list_reverse_url()
+        response = self.client.post(api_url)
+        self.assertEqual(
+            response.status_code,
+            401
+        )
+
+    def test_recipe_api_list_logged_user_can_create_a_recipe(self):
+        recipe_raw_data = self.get_recipe_raw_data()
+        auth_data = self.get_auth_data()
+        jwt_access_token = auth_data.get('jwt_access_token')
+
+        response = self.client.post(
+            self.get_recipe_list_reverse_url(),
+            data=recipe_raw_data,
+            HTTP_AUTHORIZATION=f'Bearer {jwt_access_token}'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            201
+        )
+
+    def test_recipe_api_list_logged_user_can_update_a_recipe(self):
+        # Arrange (config do test)
+        recipe = self.make_recipe()
+        access_data = self.get_auth_data(username='test_patch')
+        jwt_access_token = access_data.get('jwt_access_token')
+        author = access_data.get('user')
+        recipe.author = author
+        recipe.save()
+
+        wanted_new_title = f'The new title updated by {author.username}'
+
+        # Action (Ação)
+        response = self.client.patch(
+            reverse('recipes:recipes-api-detail', args=(recipe.id,)),
+            data={
+                'title': wanted_new_title
+            },
+            HTTP_AUTHORIZATION=f'Bearer {jwt_access_token}'
+        )
+
+        # Assertion (Afirmação)
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+        self.assertEqual(
+            response.data.get('title'),
+            wanted_new_title,
+        )
+
+    def test_recipe_api_list_logged_user_cant_update_a_recipe_owned_by_another_user(self):  # noqa
+        # Arrange (config do test)
+        recipe = self.make_recipe()
+        access_data = self.get_auth_data(username='test_patch')
+
+        # This user cannot update the recipe because it is owned by another
+        # user.
+        another_user = self.get_auth_data(username='cant_update')
+        jwt_access_token_from_another_user = another_user.get(
+            'jwt_access_token'
+        )
+
+        # This is the actual owner of the recipe
+        author = access_data.get('user')
+        recipe.author = author
+        recipe.save()
+
+        # Action (Ação)
+        response = self.client.patch(
+            reverse('recipes:recipes-api-detail', args=(recipe.id,)),
+            data={},
+            HTTP_AUTHORIZATION=f'Bearer {jwt_access_token_from_another_user}'
+        )
+
+        # Assertion (Afirmação)
+        # Another user cannot update the recipe, so the status code
+        # must be 403 Forbidden
+        self.assertEqual(
+            response.status_code,
+            403,
+        )
